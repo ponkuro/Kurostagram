01e8d07a65c99c477e1d06c8090647a04f6f1d72
Hiroyuki Kurose <pon.kurose.web@gmail.com>
2016-06-27 14:12:38 +0000

    HTML/CSSの編集

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,9 @@
 class TopController < ApplicationController
   def index
+    if user_signed_in?
+      redirect_to user_url(current_user.id)
+    else
+      redirect_to new_user_registration_url
+    end
   end
 end
