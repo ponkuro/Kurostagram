6a412c4cf7fa802add3d51c4ab3b8e85b6623ead
Hiroyuki Kurose <pon.kurose.web@gmail.com>
2016-07-17 19:44:30 +0000

    rails_adminのインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users,
     path_names: {sign_in: "login", sign_out: "logout"},
     controllers: {
